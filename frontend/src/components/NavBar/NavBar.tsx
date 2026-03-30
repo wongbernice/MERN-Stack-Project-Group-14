47506d4c06e29a36c913c0ab5f490d273bc35122
Chocoie <trevi1206@gmail.com>
2026-03-30 16:03:56 -0400

    updated navBar -trevi

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,20 +1,45 @@
 import './navBar.css'
 import duck from '../../assets/Duck_Image.png'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export const NavBar = () =>
 {
     const navigate = useNavigate();
+    const location = useLocation();
+    const hideLocations = location.pathname === '/' || location.pathname === '/login' || location.pathname === '/signUp';
+
     const handleLogoClick = () =>
+    {
+        navigate('/');
+    }
+
+    const handleDashClick = () =>
+    {
+        navigate('/dashboard');
+    }
+
+    const handleTransClick = () =>
+    {
+        navigate('/transactions');
+    }
+
+    const handleLogOut = () =>
     {
         navigate('/');
     }
 
     return(
         <>
-            <div className='navBarDiv'>
+            <nav className='navBarNav'>
                 <span id="logo" onClick={handleLogoClick}><img id="logoImg" src="../src/assets/Logo.png" alt="Logo"/></span>
-            </div>
+                { !hideLocations && (
+                    <div className='navBarOptions'>
+                        <button id="dashboardLink" onClick={handleDashClick}>Dashboard</button>
+                        <button id="transactionsLink" onClick={handleTransClick}>Transactions</button>
+                        <button id="logoutBtn">Log Out</button>
+                    </div>
+                )}
+            </nav>
         </>
     );
 };
