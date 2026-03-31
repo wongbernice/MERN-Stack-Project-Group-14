5675b856ac0dafce1455c28ca9cccf0d8717cd81
Chocoie <trevi1206@gmail.com>
2026-03-31 17:25:21 -0400

    started styling index -trevi

--- frontend/src/pages/Index/Index.tsx
+++ frontend/src/pages/Index/Index.tsx
@@ -1,15 +1,30 @@
 import { useNavigate } from 'react-router-dom'
+import { NavBar } from '../../components/NavBar/NavBar'
+import './IndexPage.css'
+import duck from '../../assets/Duck_Image.png'
 
 export const Index = () => 
 {
     const navigate = useNavigate();
     return(
         <>
-            <h1>Index</h1>
-            <div className="IndexButtons">
-                <button id="newUser" type="button" onClick={() => navigate('/signUp')}>Sign Up</button>
-                <button id="existingUser" type="button" onClick={() => navigate('/login')}>Login</button>
-            </div>
+            <NavBar />
+            <main className='indexMain'>
+                <div className='words'>
+                    <p id="header">Keep Your Ducks In A Row,<br/> and Your Budget Too</p>
+                    <p id="body">
+                        Ducky Dollars helps you set budgets and monitor your spending by
+                        <br/>
+                        category, so you can stay on track towards you financial goals.
+                    </p>
+                </div>
+                
+                <div className="indexButtons">
+                    <button id="existingUser" type="button" onClick={() => navigate('/login')}>Login</button>
+                    <button id="newUser" type="button" onClick={() => navigate('/signUp')}>Sign Up</button>
+                </div>
+            </main>
+            <img id="duckImgIndex" src={duck} alt="Duck Image" />
         </>
     );
 };
