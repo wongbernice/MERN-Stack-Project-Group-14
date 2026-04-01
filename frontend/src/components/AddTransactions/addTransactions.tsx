4a70c108f815ef8c03cb32bef84b01a642b67d4c
Chocoie <trevi1206@gmail.com>
2026-04-01 12:37:46 -0400

    added new transaction modal -trevi

--- frontend/src/components/AddTransactions/addTransactions.tsx
+++ frontend/src/components/AddTransactions/addTransactions.tsx
@@ -1,3 +1,5 @@
+import './addTransactionsPage.css'
+
 //defining items need to close the popup/overlay
 interface OverlayItems {
     onClose: () => void;
@@ -9,7 +11,21 @@ export const AddTransaction = ({onClose, onSubmit}: OverlayItems) =>
     return(
         <>
             <div className="overlay" onClick={onClose}>
-                
+                <div className='formCard' onClick={(e) => e.stopPropagation()}>
+                    <button id="closeForm" onClick={onClose}>X</button>
+                    <h3 id='addTitle'>Add Transaction</h3>
+                    <form className='newTransaction'>
+                        <input id='transDate' type='date' placeholder='Date' required/>
+                        <br/>
+                        <input id='transCategory' type='text' placeholder='Category' required/>
+                        <br/>
+                        <input id='transAmount' type='number' placeholder='Amount' required/>
+                        <br/>
+                        <textarea id='transNote' placeholder='Notes' rows={4} required/>
+                        <br/>
+                        <button id='newTransSubmit' type='submit' onClick={onSubmit}>Add</button>
+                    </form>
+                </div>
             </div>
         </>
     )
