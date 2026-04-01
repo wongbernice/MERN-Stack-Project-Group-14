cb532b661c68d9bfcf2af13e794fea4d829d0091
Victoria Valdes <vvaldes0520@gmail.com>
2026-03-31 23:43:54 -0400

    Added Budget page

--- frontend/src/pages/Budget/Budget.tsx
+++ frontend/src/pages/Budget/Budget.tsx
@@ -1,3 +1,10 @@
+// 1 Button - Add Budget Category
+    // Brings up pop up for the user to add a category name & budget amount
+    // When save is hit, 
+        // Budget Amount must be a float number
+        // Category Name must be a string
+        // Cannot save if either field is empty or inputs are invalid
+
 export const BudgetPage = () =>
 {
     return(
