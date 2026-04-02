bd1fa63acf736c146ebdb11cf773a591ab63ff69
blazingprincess <byhwong@fierysnow.com>
2026-04-02 14:26:01 -0400

    Add verify to authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authController');
+const { registerUser, loginUser, verifyCode } = require('../controllers/authController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
