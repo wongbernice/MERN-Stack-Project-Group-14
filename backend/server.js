2139c59b1ffab3eab91e85c6ca32417bce1d0c66
Chocoie <trevi1206@gmail.com>
2026-04-07 18:44:57 -0400

    updated server.js to run in browser -trevi

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,'../frontend/dist')));
+
+app.get(/^(?!\/api).+/, (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+});
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
