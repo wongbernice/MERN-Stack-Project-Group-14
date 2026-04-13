f1de0a31320e771e7df778f8fd0be28462faa8ed
Sameer Syed <reachmeatsameer@gmail.com>
2026-04-12 21:58:52 -0400

    Small changes committed to get edits from main branch into mobile branch

--- mobile/ducky_dollars/lib/main.dart
+++ mobile/ducky_dollars/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'authPages/login.dart';
 import 'authPages/signup.dart';
-import 'package:cristalyse/cristalyse.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 const ddWhite = Color(0xfffefeff);
 const ddSky = Color(0xffd6efff);
@@ -25,15 +25,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Ducky Dollars',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
       ),
       home: const MyLandingPage(title: 'Ducky Dollars'),
     );
